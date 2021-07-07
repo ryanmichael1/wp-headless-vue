657fc855ecbc16e6f18a1212141f389e1281e616
Ryan <ryan.michael@twentysixdigital.com>
2021-07-07 16:56:44 +0100

    getting data

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,29 @@
 <template>
-  <Tutorial/>
+  <div class="posts">
+    <main>
+      <h2>Posts</h2>
+      <!-- here we loop through the posts -->
+      <div class="post" v-for="post in posts" :key="post.id">
+        <h3>
+          <!-- for each one of them, we’ll render their title, and link off to their individual page -->
+          <a :href="`blog/${post.slug}`">{{ post.title.rendered }}</a>
+        </h3>
+        <div v-html="post.excerpt.rendered"></div>
+        <a :href="`blog/${post.slug}`" class="readmore">Read more ⟶</a>
+      </div>
+    </main>
+  </div>
 </template>
 
 <script>
-export default {}
+export default {
+  computed: {
+    posts() {
+      return this.$store.state.posts;
+    }
+  },
+  created() {
+    this.$store.dispatch("getPosts");
+  }
+};
 </script>
