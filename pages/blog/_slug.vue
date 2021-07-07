8f2e2a8224fddf20593b634925d7b8cf66e606b8
Ryan <ryan.michael@twentysixdigital.com>
2021-07-07 17:13:56 +0100

    test

---
+++ pages/blog/_slug.vue
@@ -0,0 +1,28 @@
+<template>
+  <main class="post individual">
+    <h1>{{ post.title.rendered }}</h1>
+    <small class="date">{{ post.date | dateformat }}</small>
+    <section v-html="post.content.rendered"></section>
+  </main>
+</template>
+
+<script>
+export default {
+  computed: {
+    posts() {
+      return this.$store.state.posts;
+    },
+    post() {
+      return this.posts.find(el => el.slug === this.slug);
+    }
+  },
+  data() {
+    return {
+      slug: this.$route.params.slug
+    };
+  },
+  created() {
+    this.$store.dispatch("getPosts");
+  }
+};
+</script>
